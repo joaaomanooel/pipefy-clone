1816c40c81916b82dba8c9ba7c2f445e4c7df3d2
João Manoel Neto <joaaomanooel@gmail.com>
2020-06-18 19:16:00 -0300

    add drag and drop to list

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   height: 80px;
   padding: 0 30px;
-  background: #7159C1;
+  background: #3B5BFD;
   color: #FFF;
   display: flex;
   align-items: center;
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { MdAdd } from 'react-icons/md';
 
+import { useDrop } from 'react-dnd';
+
+import BoardContext from '../Board/context';
 import Card from '../Card';
 
 import { Container } from './styles';
 
 export default ({ data, index: listIndex }) => {
+  const { move } = useContext(BoardContext);
+
+  const [, dropRef] = useDrop({
+    accept: 'CARD',
+    hover: (item, monitor) => {
+      const draggedListIndex = item.listIndex;
+      const targetListIndex = listIndex;
+
+      const draggedIndex = item.index;
+      const targetIndex = data.cards.length;
+      if (targetListIndex === draggedListIndex) return null;
+
+      move(draggedListIndex, targetListIndex, draggedIndex, targetIndex);
+      item.index = targetIndex; // eslint-disable-line no-param-reassign
+      item.listIndex = targetListIndex; // eslint-disable-line no-param-reassign
+      return null;
+    },
+  });
+
   return (
-    <Container done={data.done}>
+    <Container done={data.done} ref={dropRef}>
       <header>
         <h2>{data.title}</h2>
         {data.creatable && (
